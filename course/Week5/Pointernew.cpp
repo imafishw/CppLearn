3453739778b613edf721be868b869619f1541a41
imafishw <2916121156@qq.com>
2026-04-14 17:27:23 +0800

    第五章

--- course/Week5/Pointernew.cpp
+++ course/Week5/Pointernew.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cstring>
 
 typedef struct {
     char name[4];
     int born;
     bool male;
 } Student;
+
+
 
 
 int main() {
@@ -14,6 +17,12 @@ int main() {
         true
     };
     Student* p = &lzh;
+    p->name[sizeof(p->name) - 1] = '\0'; // Ensure null termination
+    std::cout << p->name << " was born in " << p->born << std::endl;
+    std::cout << "-----------------" << std::endl;
+    strncpy(p->name, "Johan", sizeof(p->name) - 1);
+    (*p).born = 2001;
+    p->male = false;
     std::cout << p->name << " was born in " << p->born << std::endl;
     return 0;
 }
